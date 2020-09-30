f567f8253e90cf6bf93e18a6229fbb506108e86b
Signior-X <b19188@students.iitmandi.ac.in>
2020-09-30 19:43:16 +0530

    Initiated, added navbar and footer, basic layout done

--- src/App.js
+++ src/App.js
@@ -2,23 +2,59 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  Button,
+  Navbar,
+  Nav, Container, Row, Col
+} from 'react-bootstrap';
+import Home from './pages/Home';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar expand="lg">
+          {/* Add logo here */}
+          <Navbar.Brand href="#home">HeyDoc</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            {/* Left Menu */}
+            <Nav className="mr-auto">
+
+            </Nav>
+
+            {/* Right Menu */}
+            <Nav>
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </header>
+
+      <main>
+        {/* Start different sections here */}
+        <Home />
+      </main>
+
+      <footer>
+        <Container>
+          <Row>
+            <Col xs lg="2" className="f-start">
+              © {new Date().getFullYear()},
+              {` `}
+              HeyDoc
+            </Col>
+            <Col className="f-mid">
+              <Row>
+                <Col md="auto">Contact</Col>
+                <Col md="auto">Help</Col>
+              </Row>
+            </Col>
+            <Col xs lg="2" className="f-end" >Some Info</Col>
+          </Row>
+        </Container>
+      </footer>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,84 @@
+import React, { Fragment } from 'react'
+
+import '../css/home.css'
+
+function Home() {
+  return (
+    <Fragment>
+      <strong className="section-title">Hellow</strong>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+      <p>Hellow</p>
+    </Fragment>
+  )
+}
+
+export default Home
